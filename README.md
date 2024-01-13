04ca377893616465670900ea309530335299c05c
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2024-01-14 01:12:33 +1300

    v0.1.4
    Fix https://github.com/cloudy-sfu/GUI-for-tesseract-OCR/issues/2

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
  The GUI for Tesseract OCR software in Windows 64-bit platform
 
 ![](https://img.shields.io/badge/OS-Windows%2010%2064--bit-lightgrey)
-![](https://img.shields.io/badge/dependencies-Python%203.10-blue)
+![](https://img.shields.io/badge/dependencies-Python%203.11-blue)
 
 ## Usage
 
+### Release
+
 1. Unzip and click `GUI-for-tesseract-OCR.exe` to run this program.
-2. By default, we provide English language model in the installation package. Click `Help | Version and supported language ` to find which language models have been installed.
+2. By default, we provide an English language model in the installation package. Click `Help | Version and supported language ` to find installed language models.
     ![image-20230212142100249](./assets/image-20230212142100249.png)
 
-If the languages you want is not supported: 
+If the languages you want are not supported: 
 
 3.   Click `File | Download pretrained language models ` to find the language models. The program will call your default web browser and direct you to the download page.
      ![image-20230212142123835](./assets/image-20230212142123835.png)
@@ -22,7 +24,7 @@ If the languages you want is not supported:
      ![image-20230212142409014](./assets/image-20230212142409014.png)
 7.   Click `File | Open language modules`. Then, the program will automatically call the file explorer and pop up a folder. Move the file you downloaded to the popped-up folder. Finally, close the file explorer.
      ![image-20230212142444241](./assets/image-20230212142444241.png)
-8.   If you want to switch the language, which OCR program uses, please click `File | Switch language` . In the popped-up dialog "Select a language", choose your language and click "OK".
+8.   If you want to switch the language, that the OCR program uses, please click `File | Switch language` . In the popped-up dialog "Select a language", choose your language and click "OK".
      ![image-20230212142823934](./assets/image-20230212142823934.png)
      The current language will appear in the "Language" row in the main window.
 
@@ -30,7 +32,16 @@ Now, you're set up and can choose any item in `Recognize` menu to start an OCR t
 
 ![image-20230212143110006](./assets/image-20230212143110006.png)
 
-## Acknowledgement
+### Source code
 
-The files in `raw/models` are copied from [Tessdata](https://github.com/tesseract-ocr/tessdata/tree/4767ea922bcc460e70b87b1d303ebdfed0897da8) which is not included in the license of this repository. The copyright is reserved by Tessdata, and we redistribute these files based on their license.
+Download the "tesseract" wheel package from https://github.com/simonflueckiger/tesserocr-windows_build/releases
 
+For example, the downloaded filename is `tesserocr-2.6.0-cp311-cp311-win_amd64.whl`. Put it at the program root directory, and run `pip install tesserocr-2.6.0-cp311-cp311-win_amd64.whl `.
+
+Then, run `pip install -r requirements.txt`.
+
+To compile a release version, run `pyinstaller main.spec`.
+
+## Acknowledgment
+
+[Tessdata](https://github.com/tesseract-ocr/tessdata/tree/4767ea922bcc460e70b87b1d303ebdfed0897da8) provides the models.
--- ocr_wrapper.py
+++ ocr_wrapper.py
@@ -1,3 +1,4 @@
+import locale
 import tesserocr
 from PIL import Image
 from PyQt5.QtCore import *
@@ -20,7 +21,8 @@ class BatchOCR(QThread):
         # get files' path
         filenames = []
         for parent, _, files in os.walk(self.input_folder):
-            filenames += [(parent, x) for x in files if os.path.splitext(x)[1].lower() in ['.jpg', '.png']]
+            filenames += [(parent, x) for x in files if os.path.splitext(x)[1].lower()
+                          in ['.jpg', '.png']]
         n_files = len(filenames)
         if n_files == 0:
             self.gui_message.emit('This folder does not contains any picture.')
@@ -32,23 +34,33 @@ class BatchOCR(QThread):
             output_image_name, output_image_ext = os.path.splitext(name)
             output_path_text = os.path.join(self.output_folder, output_image_name + '.txt')
             try:
-                text = tesserocr.file_to_text(filepath, oem=tesserocr.OEM.TESSERACT_ONLY, path='raw/models',
-                                              lang=self.lang)
+                text = tesserocr.file_to_text(filepath, oem=tesserocr.OEM.TESSERACT_ONLY,
+                                              path='raw/models', lang=self.lang)
             except Exception as e:
                 self.gui_message.emit(f'{filepath} - {e}')
-                self.gui_message.emit('Maybe you have not imported a language model. Please follow the usage at '
-                                      'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR and import your first '
-                                      'language model.')
+                self.gui_message.emit('Maybe you have not imported a language model. '
+                                      'Please follow the usage at '
+                                      'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR '
+                                      'and import your first language model.')
                 self.progress.emit((i + 1, n_files))
                 continue
             if not text:
                 self.gui_message.emit(f'{filepath} doesn\'t contain text.')
-            with open(output_path_text, 'w') as f:
-                f.write(text)
+            try:
+                with open(output_path_text, 'w') as f:
+                    f.write(text)
+            except UnicodeEncodeError:
+                self.gui_message.emit(f'{filepath} - Unicode encode error.')
+                self.gui_message.emit("The image contains characters that cannot be "
+                                      "encoded by the character set of your operation "
+                                      f"system, {locale.getencoding()}.")
+                self.gui_message.emit("Please try recognizing this file in \"Single "
+                                      "File\" mode.")
             del text
             progress_percentage = int((i + 1) / n_files * 100)
             self.progress.emit(progress_percentage)
-        self.gui_message.emit('Click "File | Open target folder" in menu bar to view the exported images.')
+        self.gui_message.emit('Click "File | Open target folder" in the menu bar to view '
+                              'the exported images.')
         self.done.emit(True)
 
 
@@ -64,13 +76,15 @@ class SingleOCR(QThread):
 
     def run(self) -> None:
         try:
-            text = tesserocr.file_to_text(self.input_file, oem=tesserocr.OEM.TESSERACT_ONLY, path='raw/models',
-                                          lang=self.lang)
+            text = tesserocr.file_to_text(self.input_file,
+                                          oem=tesserocr.OEM.TESSERACT_ONLY,
+                                          path='raw/models', lang=self.lang)
         except Exception as e:
             self.gui_message.emit(f'{self.input_file} - {e}')
-            self.gui_message.emit('Maybe you have not imported a language model. Please follow the usage at '
-                                  'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR and import your first language '
-                                  'model.')
+            self.gui_message.emit('Maybe you have not imported a language model. Please '
+                                  'follow the usage at '
+                                  'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR '
+                                  'and import your first language model.')
             self.done.emit(True)
             return
         self.gui_message.emit(text)
@@ -87,7 +101,8 @@ def pixmap_to_pillow_image(pixmap):
     if q_image.bits() is None:
         return
     buffer = q_image.bits().asarray(q_image.byteCount())
-    pillow_image = Image.frombytes("RGBA", (width, height), buffer, "raw", "BGRA")
+    pillow_image = Image.frombytes("RGBA", (width, height),
+                                   buffer, "raw", "BGRA")
     return pillow_image
 
 
@@ -103,13 +118,15 @@ class ClipboardOCR(QThread):
 
     def run(self) -> None:
         try:
-            text = tesserocr.image_to_text(self.pillow_image, oem=tesserocr.OEM.TESSERACT_ONLY, path='raw/models',
-                                           lang=self.lang)
+            text = tesserocr.image_to_text(self.pillow_image,
+                                           oem=tesserocr.OEM.TESSERACT_ONLY,
+                                           path='raw/models', lang=self.lang)
         except Exception as e:
             self.gui_message.emit(e.__str__())
-            self.gui_message.emit('Maybe you have not imported a language model. Please follow the usage at '
-                                  'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR and import your first language '
-                                  'model.')
+            self.gui_message.emit('Maybe you have not imported a language model. Please '
+                                  'follow the usage at '
+                                  'https://github.com/cloudy-sfu/GUI-for-tesseract-OCR '
+                                  'and import your first language model.')
             self.done.emit(True)
             return
         self.gui_message.emit(text)
